abb08df1a8e11acca1ac8ef575ec168e23ce6fb7
SANDEEP7522 <159306588+SANDEEP7522@users.noreply.github.com>
2024-07-21 12:24:08 +0530

    create url name part

--- script.js
+++ script.js
@@ -1,16 +1,40 @@
+
+
 const tierInput = document.getElementById('tier');
 console.log(tierInput);
 
-const submitBtn = document.getElementById('sumit');
+const submitBtn = document.getElementById('submit');
+
+const imgForm = document.getElementById('img-form');
+
+imgForm.addEventListener('click', (event) => {
+    event.preventDefault();
+    console.log("form submitted");
+
+    const imgItemInput = document.getElementById('img-item');
+    
+    const imageUrl = imgItemInput.value;
+    console.log(imageUrl);
+
+    createTierListItemUrl(imageUrl);
+});
+
 submitBtn.addEventListener('click', (event) => {
     console.log("button is clicked");
-    event.preventDefault();// stops the default behaviour of the event
+    event.preventDefault(); // stops the default behaviour of the event
 
     // to get access of the element on which this event was fired
     const target = event.target;
     console.log(tierInput.value);
+
+    if (tierInput.value === '') {
+        alert('Please enter a tier name');
+        return;
+    }
     createTierList(tierInput.value);
+    tierInput.value = '';
 });
+
 function createTierList(tierListName) {
     const newTierList = document.createElement('div');
     newTierList.classList.add('tier-list');
@@ -19,17 +43,27 @@ function createTierList(tierListName) {
     heading.textContent = tierListName;
     newTierList.appendChild(heading);
 
+    const newTierListItems = document.createElement('div');
+    newTierListItems.classList.add('tier-list-items');  
+    newTierList.appendChild(newTierListItems); 
 
-     const newTierListitems = document.createElement('div');
-     newTierListitems.classList.add('tier-list-items');  
-     newTierList.appendChild(newTierListitems); 
-    
-     const newTierListitemsForLine = document.createElement('div');
-     newTierListitemsForLine.classList.add('line');
-     newTierList.appendChild(newTierListitemsForLine);
+    const newTierListItemsForLine = document.createElement('div');
+    newTierListItemsForLine.classList.add('line');
+    newTierList.appendChild(newTierListItemsForLine);
 
+    const tierSection = document.getElementById('tier-list-section');
+    tierSection.appendChild(newTierList);
+}
 
-     const tierSection = document.getElementById
-     ('tier-list-section');
-     tierSection.appendChild(newTierList);
+function createTierListItemUrl(imageUrl) {
+    const imageDiv = document.createElement('div');
+    imageDiv.classList.add('item-container');
+
+    const img = document.createElement('img');
+    img.src = imageUrl;
+
+    imageDiv.appendChild(img);
+
+    const nonTierSection = document.getElementById('item-section-container');
+    nonTierSection.appendChild(imageDiv);
 }
